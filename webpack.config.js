3a8cecf68b3b0f12f0a9b1b90cc94a8e94adbb2e
drkchu <derekdbchu@gmail.com>
2024-04-09 10:34:38 -0700

    fix: set up webpack template for this project

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      favicon: "./src/assets/img/mostly_sunny.png",
+      favicon: "./src/assets/img/ship.png",
       template: "./src/index.html",
     }),
   ],
